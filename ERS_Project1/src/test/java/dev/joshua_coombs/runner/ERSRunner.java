3cdc6824509dafadd2d5d5c7bc4f60259c365de6
joshuacoombs <h0wlpenwyrm@gmail.com>
2022-06-22 13:08:59 -0700

    adding updates to skeleton of project 1

--- ERS_Project1/src/test/java/dev/joshua_coombs/runner/ERSRunner.java
+++ ERS_Project1/src/test/java/dev/joshua_coombs/runner/ERSRunner.java
@@ -14,7 +14,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources"}, glue = {"com.revature.steps"})
+@CucumberOptions(features = {"src/test/resources"}, glue = {"dev.joshua_coombs.steps"})
 public class ERSRunner {
 	public static WebDriver realHumanBeing;
 	public static ERSPage ersPage;
